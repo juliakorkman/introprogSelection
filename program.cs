8b285714f10b853c698ab118efc268a87ff7c3c6
juliakorkman <61397175+juliakorkman@users.noreply.github.com>
2020-03-03 14:53:35 +1100

    Add files via upload

---
+++ program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace introprogSelection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            age = 18;
+            int lAge = 18;
+
+            if (age >= lAge) 
+            {
+                Console.WriteLine("Have a Drink Buddy");
+                Console.WriteLine("Get your license bruh");
+
+            }
+            else 
+            {
+                Console.WriteLine("No Drinking for you buddy");
+                Console.WriteLine("Get your hours up bro");
+
+            }
+
+            Console.WriteLine("completed uwu");
+            {
+                
+            }
+        }
+    }
+}
